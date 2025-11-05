7611610bc13fc66f2d136861b0235cad2f8a2d87
agusfussi <agusfussiwor@gmail.com>
2025-11-05 12:54:40 -0300

    logged-header

--- src/app/layauot/logged-header/logged-header.ts
+++ src/app/layauot/logged-header/logged-header.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-logged-header',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './logged-header.html',
-  styleUrl: './logged-header.scss',
+  styleUrls: ['./logged-header.scss'],
 })
 export class LoggedHeader {
+authService = inject(AuthService);
 
+getuserName(): string | undefined {
+  return undefined;
+}
+
+logout() {
+    this.authService.logout();
+  }
 }
