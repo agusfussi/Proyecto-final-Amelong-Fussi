e2332c6e7998b8a1ceee595f0cc57c8262a53ad5
agusfussi <agusfussiwor@gmail.com>
2025-11-05 11:10:30 -0300

    types nuevos

---
+++ src/app/interfaces/categories-types.ts
@@ -0,0 +1,3 @@
+export interface CategoriesType{
+  name: string
+}
---
+++ src/app/interfaces/products-types.ts
@@ -0,0 +1,11 @@
+export interface ProductType{
+  name: string,
+  description: string,
+  price: 0,
+  categoryId: 0,
+  featured: true,
+  labels: [],
+  recommendedFor: 0,
+  discount: 0,
+  hasHappyHour: true
+}
